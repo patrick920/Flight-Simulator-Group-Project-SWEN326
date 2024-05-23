09934c8c6b8cbb2915513e4a83b1c05214a8c0e5
nickwinsley <140232151+nickwinsley@users.noreply.github.com>
2024-05-23 13:56:52 +1200

    Added 2oo3 redundant architecture functionality to Simulator

--- src/main/java/org/swen326/simulator/Simulator.java
+++ src/main/java/org/swen326/simulator/Simulator.java
@@ -1,5 +1,11 @@
 package org.swen326.simulator;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+
 import org.swen326.simulator.map.Flight;
 import org.swen326.simulator.sensors.Ailerons;
 import org.swen326.simulator.sensors.Elevator;
@@ -14,10 +20,15 @@ import org.swen326.simulator.sensors.Sensor;
  */
 public class Simulator {
 
-    public static Sensor sensor;
+    public static List<Sensor> aileron_sensors;
+    public static List<Sensor> elevator_sensors;
+    public static List<Sensor> rudder_sensors;
     public Ailerons aileron;
     public Elevator elevator;
     public Rudder rudder;
+    public boolean aileron_redundancy;
+    public boolean rudder_redundancy;
+    public boolean elevator_redundancy;
 
     /**
      * @param maximum_thrust - Maximum thrust of plane model
@@ -26,14 +37,23 @@ public class Simulator {
      * "Start Simulation" button is clicked.
      */
     public void runSimulator(double maximum_thrust, double minimum_thrust){
+        Simulator.rudder_sensors = new ArrayList<Sensor>();
+        Simulator.elevator_sensors = new ArrayList<Sensor>();
+        Simulator.aileron_sensors = new ArrayList<>();
         System.out.println("Run Simulation");
-        Sensor sens = new Sensor();
-        Simulator.sensor = sens;
+        for (int i = 0; i < 3; i++){
+            rudder_sensors.add(new Sensor());
+            elevator_sensors.add(new Sensor());
+            aileron_sensors.add(new Sensor());
+        }
         aileron = Ailerons.getAileron();
         rudder = Rudder.getRudder();
         elevator = Elevator.getElevator();
         Plane.maxThrust = maximum_thrust;
         Plane.minThrust = minimum_thrust;
+        aileron_redundancy = true;
+        rudder_redundancy = true;
+        elevator_redundancy = true;
     }
 
     /**
@@ -57,5 +77,76 @@ public class Simulator {
 
     public void setYaw(double yaw){
         Plane.setYaw(yaw, 0.1);
+    }
+
+    public double getRoll(){
+        Map<Double, Integer> vals = new HashMap<>();
+        
+        for (Sensor sens : Simulator.aileron_sensors){
+            double val = sens.getRoll();
+            if (!vals.keySet().contains(val)){
+                vals.put(val, 1);
+            }
+            else {
+                vals.put(val, vals.get(val) + 1);
+            }
+        }
+
+        if (vals.keySet().size() != 1){
+            aileron_redundancy = false;
+        }
+
+        else {
+            aileron_redundancy = true;
+        }
+
+        return Collections.max(vals.entrySet(), Map.Entry.comparingByValue()).getKey();
+    }
+
+    public double getYaw(){
+        Map<Double, Integer> vals = new HashMap<>();
+        
+        for (Sensor sens : Simulator.rudder_sensors){
+            double val = sens.getYaw();
+            if (!vals.keySet().contains(val)){
+                vals.put(val, 1);
+            }
+            else {
+                vals.put(val, vals.get(val) + 1);
+            }
+        }
+
+        if (vals.keySet().size() != 1){
+            rudder_redundancy = false;
+        }
+
+        else {
+            rudder_redundancy = true;
+        }
+
+        return Collections.max(vals.entrySet(), Map.Entry.comparingByValue()).getKey();
+    }
+
+    public double getPitch(){
+        Map<Double, Integer> vals = new HashMap<>();
+        
+        for (Sensor sens : Simulator.elevator_sensors){
+            double val = sens.getYaw();
+            if (!vals.keySet().contains(val)){
+                vals.put(val, 1);
+            }
+            else {
+                vals.put(val, vals.get(val) + 1);
+            }
+        }
+
+        if (vals.keySet().size() != 1){
+            elevator_redundancy = false;
+        }
+        else {
+            elevator_redundancy = true;
+        }
+
+        return Collections.max(vals.entrySet(), Map.Entry.comparingByValue()).getKey();
     }
 }
