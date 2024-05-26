6cd35f11b57836c0dd96f357bea740b9b418df67
millspatr1 <millspatr1@ecs.vuw.ac.nz>
2024-05-27 11:18:11 +1200

    Added a test folder with one test file.

---
+++ src/test/java/org/swen326/userinterface/HomePageTest.java
@@ -0,0 +1,24 @@
+package org.swen326.userinterface;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HomePageTest {
+
+    @Test
+    void getSelectedAircraftType() {
+    }
+
+    @Test
+    void startSimulationButtonClicked() {
+    }
+
+    @Test
+    void displayInvalidInputMessage() {
+    }
+
+    @Test
+    void display() {
+    }
+}
