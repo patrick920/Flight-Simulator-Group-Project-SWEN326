8fcc8230439869eb58098e6814a16f3ce0e17416
sawyerpatr1 <sawyerpatr1@ecs.vuw.ac.nz>
2024-05-27 11:08:44 +1200

    added stuff

--- src/main/java/org/swen326/userinterface/ArtificialHorizon.java
+++ src/main/java/org/swen326/userinterface/ArtificialHorizon.java
@@ -1,5 +1,6 @@
 package org.swen326.userinterface;
 
+import javafx.application.Platform;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
@@ -23,43 +24,50 @@ public class ArtificialHorizon extends Canvas {
         draw();
     }
 
-    public void setPitch(double pitch) {
+    public synchronized void setPitch(double pitch) {
+        if (Double.isNaN(pitch) || Double.isInfinite(pitch)) {
+            throw new IllegalArgumentException("Pitch must be a finite number");
+        }
         this.pitch = pitch;
         draw();
     }
 
-    public void setRoll(double roll) {
+    public synchronized void setRoll(double roll) {
+        if (Double.isNaN(roll) || Double.isInfinite(roll)) {
+            throw new IllegalArgumentException("Roll must be a finite number");
+        }
         this.roll = roll;
         draw();
     }
 
     private void draw() {
-        double width = getWidth();
-        double height = getHeight();
-        GraphicsContext gc = getGraphicsContext2D();
+        Platform.runLater(() -> {
+            double width = getWidth();
+            double height = getHeight();
+            GraphicsContext gc = getGraphicsContext2D();
 
-        gc.setFill(Color.BLACK);
-        gc.fillRect(0, 0, width, height);
+            // Clear the canvas
+            gc.setFill(Color.BLACK);
+            gc.fillRect(0, 0, width, height);
 
-        gc.save();
-        Rotate rotate = new Rotate(-roll, width / 2, height / 2);
-        gc.setTransform(rotate.getMxx(), rotate.getMyx(), rotate.getMxy(), rotate.getMyy(), rotate.getTx(), rotate.getTy());
+            gc.save();
+            Rotate rotate = new Rotate(-roll, width / 2, height / 2);
+            gc.setTransform(rotate.getMxx(), rotate.getMyx(), rotate.getMxy(), rotate.getMyy(), rotate.getTx(), rotate.getTy());
 
-        gc.setFill(Color.SKYBLUE);
-        gc.fillRect(0, 0, width, height / 2 - pitch);
-        gc.setFill(Color.BURLYWOOD);
-        gc.fillRect(0, height / 2 - pitch, width, height);
+            gc.setFill(Color.SKYBLUE);
+            gc.fillRect(0, 0, width, height / 2 - pitch);
+            gc.setFill(Color.BURLYWOOD);
+            gc.fillRect(0, height / 2 - pitch, width, height);
 
-        gc.setStroke(Color.WHITE);
-        gc.setLineWidth(2);
-        gc.strokeLine(0, height / 2 - pitch, width, height / 2 - pitch);
-        gc.restore();
+            gc.setStroke(Color.WHITE);
+            gc.setLineWidth(2);
+            gc.strokeLine(0, height / 2 - pitch, width, height / 2 - pitch);
+            gc.restore();
 
-        // Draw the fixed markings
-        gc.setStroke(Color.RED);
-        gc.strokeLine(width / 2 - 10, height / 2, width / 2 + 10, height / 2);
-        gc.strokeLine(width / 2, height / 2 - 10, width / 2, height / 2 + 10);
-
-        
+            // Draw the fixed markings
+            gc.setStroke(Color.RED);
+            gc.strokeLine(width / 2 - 10, height / 2, width / 2 + 10, height / 2);
+            gc.strokeLine(width / 2, height / 2 - 10, width / 2, height / 2 + 10);
+        });
     }
 }
